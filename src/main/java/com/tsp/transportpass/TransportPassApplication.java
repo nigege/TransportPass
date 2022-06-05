4cc640252174ae601177e666b8556ff5d9aa055e
renren <314534761@qq.com>
2022-06-05 11:14:12 +0800

    Initial commit

---
+++ src/main/java/com/tsp/transportpass/TransportPassApplication.java
@@ -0,0 +1,13 @@
+package com.tsp.transportpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsp/transportpass/TransportPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsp.transportpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
